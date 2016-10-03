8cdc459e09dd8f3a26ac1239584d10976352e62f
Josh Terrell <joshterrell805@gmail.com>
2016-10-03 14:10:18 -0700

    Load SparkContext from Beans.xml and read textfile path from cli

--- src/main/java/com/joshterrell/Main.java
+++ src/main/java/com/joshterrell/Main.java
@@ -1,24 +1,59 @@
 package com.joshterrell;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import scala.Tuple2;
 
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by josh on 10/3/16.
  */
 
 public class Main {
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setMaster("local").setAppName("javatest");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+    protected JavaSparkContext sc;
 
-        JavaRDD<String> lines = sc.textFile("some_text_file.txt");
+    public Main(JavaSparkContext sc) {
+        this.sc = sc;
+    }
+
+    public static void main(String[] args) {
+        Namespace ns = parseArgs(args);
+        String path = ns.getString("file");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Main main = (Main) context.getBean("main");
+
+        main.run(path);
+    }
+
+    public static Namespace parseArgs(String[] args) {
+        ArgumentParser parser = ArgumentParsers.newArgumentParser("count words")
+                .defaultHelp(true)
+                .description("count the unique words in a text file");
+        parser.addArgument("file")
+                .help("path to a text file");
+
+        return parser.parseArgsOrFail(args);
+    }
+
+    public static SparkConf createConf(String master, String appName) {
+        return new SparkConf()
+                .setMaster(master)
+                .setAppName(appName);
+    }
+
+    public void run(String textFilePath) {
+        JavaRDD<String> lines = sc.textFile(textFilePath);
         JavaRDD<String> words = lines.flatMap((line) -> {
             String[] lineWords = line.split(" ");
             return Arrays.asList(lineWords).iterator();
